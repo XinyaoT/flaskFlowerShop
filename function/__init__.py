c3a988c9f8fe869ae160aff6f6cce5b675586bbf
Smalltown <1255432396@qq.com>
2023-05-27 00:47:52 +0800

    存放功能函数的包，每人有一个文件夹用于放自己后端写的代码

---
+++ function/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2023/5/26 21:16
+# @Author  : Smalltown
+# @FileName: __init__.py.py
+# @Software: PyCharm
---
+++ function/function_txy/shoppingcart_utils.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2023/5/27 0:46
+# @Author  : Smalltown
+# @FileName: shoppingcart_utils.py
+# @Software: PyCharm
