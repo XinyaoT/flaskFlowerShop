ec8f054e1b7e8d6286f05cf051132087cf326050
Smalltown <1255432396@qq.com>
2023-05-27 00:39:17 +0800

    app用于展示主界面

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from blueprint.clientOrders import bp as orders_client_bp
+from blueprint.clientAddress import bp as address_client_bp
+from blueprint.clientShoppingcart import bp as shoppingcart_client_bp
+from blueprint.client import bp as client_bp
 
 app = Flask(__name__)
+
+app.register_blueprint(orders_client_bp)
+app.register_blueprint(address_client_bp)
+app.register_blueprint(shoppingcart_client_bp)
+app.register_blueprint(client_bp)
 
 
 @app.route('/')
@@ -9,4 +18,4 @@ def hello_world():  # put application's code here
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
