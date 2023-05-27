0513db43ffb6487ae13239493caca4ff731e4532
Yani1214 <1508368722@qq.com>
2023-05-27 21:27:48 +0800

    用户登录和注册

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from blueprint.clientOrders import bp as orders_client_bp
 from blueprint.clientAddress import bp as address_client_bp
 from blueprint.clientShoppingcart import bp as shoppingcart_client_bp
+from blueprint.admin import bp as admin_bp
 from blueprint.client import bp as client_bp
 from flask import Flask,render_template
 
@@ -14,7 +15,7 @@ app.register_blueprint(orders_client_bp)
 app.register_blueprint(address_client_bp)
 app.register_blueprint(shoppingcart_client_bp)
 app.register_blueprint(client_bp)
-
+app.register_blueprint(admin_bp)
 
 @app.route('/')
 def hello_world():  # put application's code here
